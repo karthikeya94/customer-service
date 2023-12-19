c2871e22dfcd24e277f008e378260bf269734d73
karthikeya94 <karthikeya94@yahoo.com>
2023-12-19 14:51:05 +0530

    initial commit

---
+++ src/main/java/com/customer/payment/demo/CustomerPaymentMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.customer.payment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPaymentMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPaymentMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer/payment/demo/CustomerPaymentMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.payment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPaymentMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
